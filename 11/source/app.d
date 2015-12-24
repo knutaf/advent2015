e16e4f2127fc136f7af4e0ed3ffe2dd864d8b43c
knutaf <knutaf@gmail.com>
2015-12-24 13:12:22 -0800

    solution for day 11

--- 11/source/app.d
+++ 11/source/app.d
@@ -1,4 +1,7 @@
 import std.stdio;
+import std.regex;
+import std.format;
+import std.string;
 
 void main(string[] args)
 {
@@ -9,14 +12,66 @@ void main(string[] args)
 
     char[] input = args[1].dup();
 
-    input[input.length - 1]++;
-    for (ulong i = input.length - 1; i > 0; i--)
+    auto rxIOL = ctRegex!(`[iol]`);
+    auto rxPair = ctRegex!(`([a-z])\1`);
+
+    bool valid = false;
+    while (!valid)
     {
-        if (input[i] > 'z')
+        //writefln("trying %s", input);
+
+        input[input.length - 1]++;
+        for (ulong i = input.length - 1; i > 0; i--)
         {
-            input[i] = 'a';
-            input[i-1]++;
+            if (input[i] > 'z')
+            {
+                input[i] = 'a';
+                input[i-1]++;
+            }
         }
+
+        bool hasIOL = !!matchFirst(input, rxIOL);
+
+        bool hasSequence = false;
+        foreach (ulong i; 0 .. input.length - 2)
+        {
+            if (input[i+1] == input[i] + 1 &&
+                input[i+2] == input[i] + 2)
+            {
+                //writefln("matched %s %s %s", input[i], input[i+1], input[i+2]);
+                hasSequence = true;
+            }
+        }
+
+        bool hasPairs = false;
+        Captures!(char[]) cap = matchFirst(input, rxPair);
+        if (cap)
+        {
+            Regex!char rxOtherPair = regex(format("([^%s])\\1", cap[1]));
+            if (matchFirst(input, rxOtherPair))
+            {
+                hasPairs = true;
+            }
+        }
+
+        /*
+        if (hasIOL)
+        {
+            writeln("failed - has IOL");
+        }
+
+        if (!hasSequence)
+        {
+            writeln("failed - no sequence");
+        }
+
+        if (!hasPairs)
+        {
+            writeln("failed - doesn't have pairs");
+        }
+        */
+
+        valid = !hasIOL && hasSequence && hasPairs;
     }
 
     writefln("pw: %s", input);
