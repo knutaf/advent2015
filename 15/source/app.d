e2dc8d061d7a05aaa2e5cdf5048b56baf9acdf61
knutaf <knutaf@gmail.com>
2015-12-25 20:30:51 -0800

    solution for day 15

--- 15/source/app.d
+++ 15/source/app.d
@@ -3,19 +3,23 @@ import std.regex;
 import std.string;
 import std.format;
 import std.conv;
+import std.algorithm;
+
+struct Property
+{
+    string name;
+    int value;
+}
 
 struct Ingredient
 {
     string name;
-    int capacity;
-    int durability;
-    int flavor;
-    int texture;
+    Property[] props;
     int calories;
 
     const pure string toString()
     {
-        return format("%s: capacity %s, durability %s, flavor %s, texture %s, calories %s", name, capacity, durability, flavor, texture, calories);
+        return format("%s: %s, calories: %s", name, props, calories);
     }
 }
 
@@ -32,10 +36,10 @@ void main()
         {
             Ingredient ing;
             ing.name = cap[1];
-            ing.capacity = to!int(cap[2]);
-            ing.durability = to!int(cap[3]);
-            ing.flavor = to!int(cap[4]);
-            ing.texture = to!int(cap[5]);
+            ing.props ~= Property("capacity", to!int(cap[2]));
+            ing.props ~= Property("durability", to!int(cap[3]));
+            ing.props ~= Property("flavor", to!int(cap[4]));
+            ing.props ~= Property("texture", to!int(cap[5]));
             ing.calories = to!int(cap[6]);
 
             ingredients ~= ing;
@@ -49,5 +53,69 @@ void main()
     foreach (const ref Ingredient ing; ingredients)
     {
         writefln("ing: %s", ing);
+    }
+
+    uint[] amounts;
+    amounts.length = ingredients.length;
+    foreach (ref uint amount; amounts)
+    {
+        amount = 0;
+    }
+
+    uint[] maxAmounts;
+    int maxScore = -int.max;
+
+    immutable uint maxTotal = 100;
+    immutable uint targetCalorieScore = 500;
+
+    while (amounts[0] <= maxTotal)
+    {
+        amounts[amounts.length - 1]++;
+        for (ulong i = amounts.length - 1; i > 0; i--)
+        {
+            if (amounts[i] > maxTotal)
+            {
+                amounts[i] = 0;
+                amounts[i-1]++;
+            }
+        }
+
+        int score = 1;
+        if (sum(amounts) == maxTotal)
+        {
+            int caloriesScore = 0;
+            foreach (ulong j; 0 .. amounts.length)
+            {
+                caloriesScore += cast(int)amounts[j] * ingredients[j].calories;
+            }
+
+            if (caloriesScore == targetCalorieScore)
+            {
+                for (uint i = 0; score != 0 && i < ingredients[0].props.length; i++)
+                {
+                    int propScore = 0;
+
+                    for (uint j = 0; score != 0 && j < amounts.length; j++)
+                    {
+                        propScore += cast(int)amounts[j] * ingredients[j].props[i].value;
+                    }
+
+                    if (propScore < 0)
+                    {
+                        propScore = 0;
+                    }
+
+                    score *= propScore;
+                }
+            }
+        }
+
+        if (score > maxScore)
+        {
+            maxScore = score;
+            maxAmounts = amounts.dup;
+
+            writefln("new max score %s: %s", maxScore, maxAmounts);
+        }
     }
 }
