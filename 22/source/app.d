0eda28458e365aa8b0879d077db628a3ce95b581
knutaf <knutaf@gmail.com>
2016-01-15 17:52:17 -0800

    day 22 skeleton

---
+++ 22/source/app.d
@@ -0,0 +1,68 @@
+import std.stdio;
+import std.conv;
+import std.string;
+import std.regex;
+import std.algorithm;
+import std.array;
+import std.math;
+
+struct Item
+{
+    string name;
+    uint cost;
+    uint damage;
+    uint armor;
+}
+
+void trySetUintFromRx(R)(string str, R rx, ref uint output)
+{
+    Captures!string cap = matchFirst(str, rx);
+    if (cap)
+    {
+        output = to!uint(cap[1]);
+    }
+}
+
+void main()
+{
+    auto rxHitPoints = ctRegex!`^Hit Points: (\d+)$`;
+    auto rxDamage = ctRegex!`^Damage: (\d+)$`;
+    uint bossHitPoints = 0;
+    uint bossDamageStat = 0;
+    foreach (char[] l; stdin.byLine())
+    {
+        string line = l.idup();
+        trySetUintFromRx(line, rxHitPoints, bossHitPoints);
+        trySetUintFromRx(line, rxDamage, bossDamageStat);
+    }
+
+    writefln("boss hp: %s, damage: %s", bossHitPoints, bossDamageStat);
+
+    if (bossHitPoints == 0 ||
+        bossDamageStat == 0)
+    {
+        throw new Exception("missing required boss params!");
+    }
+
+    immutable uint magicMissileCost = 53;
+    immutable uint magicMissileDamage = 4;
+
+    immutable uint drainCost = 73;
+    immutable uint drainDamage = 2;
+    immutable uint drainHeal = 2;
+
+    immutable uint shieldCost = 113;
+    immutable uint shieldArmorBoost = 7;
+    immutable uint shieldTurns = 6;
+
+    immutable uint poisonCost = 173;
+    immutable uint poisonDamage = 3;
+    immutable uint poisonTurns = 6;
+
+    immutable uint rechargeCost = 229;
+    immutable uint rechargeGain = 101;
+    immutable uint rechargeTurns = 5;
+
+    immutable uint playerInitialHp = 50;
+    immutable uint playerInitialMana = 500;
+}
